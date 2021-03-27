d2a42dab3859fc46a8d71ae81aa1e06494b0d63a
Vizzzka <usachevavika171@gmail.com>
2021-03-27 23:42:48 +0200

    Added time buckets tables, fixed REST API

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
-import json
 from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
@@ -19,7 +18,7 @@ def home():
     return jsonify(rows)
 
 
-# Q1 -Q2
+# Q1 -Q2  /products/B00EFDZ288/reviews or  /products/B00EFDZ288/reviews?star_rating=5
 @app.route('/products/<product_id>/reviews')
 def reviews_by_product_id(product_id):
     star_rating = request.args.get('star_rating')
@@ -32,7 +31,8 @@ def reviews_by_product_id(product_id):
     rows = [row for row in rows]
     return jsonify(rows)
 
-#Q3
+
+#Q3 /customers/47293563/reviews
 @app.route('/customers/<customer_id>/reviews')
 def reviews_by_customer_id(customer_id):
     rows = session.execute("SELECT JSON review_id FROM reviews.reviews_by_customer_id "
@@ -41,7 +41,28 @@ def reviews_by_customer_id(customer_id):
     rows = [row for row in rows]
     return jsonify(rows)
 
-#Q4
+
+#Q4 /products?sorted_by_reviews=1&start_date=2013-08-01&end_date=2013-10-01&limit=20
+@app.route('/products')
+def most_reviewed_items_in_date_range():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    sorted_by_reviews = request.args.get('sorted_by_reviews')
+    limit = request.args.get('limit')
+
+
+#Q5, Q7-Q8
+# Q5 /customers?sorted_by_reviews=1&start_date=2013-08-01&end_date=2013-10-01&limit=20
+# Q7 /customers?sorted_by_negative_reviews=1&start_date=2013-08-01&end_date=2013-10-01&limit=20
+# Q8 /customers?sorted_by_positive_reviews=1&start_date=2013-08-01&end_date=2013-10-01&limit=20
+@app.route('/customers')
+def most_productive_customers_in_date_range():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    sorted_by_reviews = request.args.get('sorted_by_reviews')
+    sorted_by_negative_reviews = request.args.get('sorted_by_negative_reviews')
+    sorted_by_positive_reviews = request.args.get('sorted_by_positive_reviews')
+    limit = request.args.get('limit')
 
 
 if __name__ == '__main__':
--- fill_db.py
+++ fill_db.py
@@ -33,19 +33,31 @@ if __name__ == '__main__':
     strCQL_3 = "INSERT INTO reviews.reviews_by_customer_id (customer_id, review_id) VALUES (?,?);"
     pStatement_3 = session.prepare(strCQL_3)
 
-    strCQL_4 = "INSERT INTO reviews.item_reviews_by_date (year_month, review_date, item_id, review_id)" \
+    strCQL_4 = "INSERT INTO reviews.item_reviews_by_date (year_month, review_date, product_id, review_id)" \
                " VALUES (?,?,?,?);"
     pStatement_4 = session.prepare(strCQL_4)
 
+    strCQL_5 = "INSERT INTO reviews.customer_reviews_by_date (year_month, verified_purchase, review_date," \
+               " customer_id, review_id)" \
+               " VALUES (?,?,?,?,?);"
+    pStatement_5 = session.prepare(strCQL_5)
+
+    strCQL_7_8 = "INSERT INTO reviews.customers_reviews_by_date_and_star_rating" \
+                 " (year_month, star_rating, review_date, customer_id, review_id)" \
+                 " VALUES (?,?,?,?,?);"
+    pStatement_7_8 = session.prepare(strCQL_7_8)
+
     for index, row in df.iterrows():
-        if len(row) != 15 or index == 0:
+        if len(row) != 15:
             skipped_rows += 1
             continue
+        year_month = '-'.join(row[14].split('-')[:2])
         #session.execute(pStatement_1, [row[3], row[2]])
         #session.execute(pStatement_2, [row[3], row[7], row[2]])
         #session.execute(pStatement_3, [row[1], row[2]])
-        year_month = row[14]    #change
-        session.execute(pStatement_4, [year_month, row[14], row[3], row[2]])
+        #session.execute(pStatement_4, [year_month, row[14], row[3], row[2]])
+        #session.execute(pStatement_5, [year_month, row[11], row[14], row[1], row[2]])
+        session.execute(pStatement_7_8, [year_month, row[7], row[14], row[1], row[2]])
 
         if index % 10000 == 0:
             print("Row processed: {}".format(index))
